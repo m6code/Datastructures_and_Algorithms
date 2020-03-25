f2de199ff2350680dfb089247093adaa8eb0dfd3
M6code <benjamynfrank101@gmail.com>
2020-03-25 21:06:39 +0100

    Working with Selection Sort Algorithm

--- SelectionSort/src/com/m6code/Main.java
+++ SelectionSort/src/com/m6code/Main.java
@@ -3,35 +3,37 @@ package com.m6code;
 public class Main {
 
     public static void main(String[] args) {
-
         int[] intArray = {20, 25, -15, 7, 55, 1, -22};
 
 
         System.out.println("Array Before Sorting ");
-        for (int i = 0; i< intArray.length; i++){
-            System.out.print(" "+ intArray[i]);
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.print(intArray[i] + " ");
         }
 
+
+        // Implement SelectionSort Logic Here
         for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
 
-            for (int i = 0; i < lastUnsortedIndex; i++) {
-                if (intArray[i] > intArray[i + 1]) {
-                    swap(intArray, i, i + 1);
+            int largest = 0;
+
+            for (int i = 1; i <= lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[largest]) {
+                    largest = i;
                 }
             }
+
+            swap(intArray, largest, lastUnsortedIndex);
         }
 
         System.out.println("\nArray After Sorting ");
-        for (int i = 0; i< intArray.length; i++){
-            System.out.print(" "+ intArray[i]);
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.print(intArray[i] + " ");
         }
-
     }
 
-
     public static void swap(int[] array, int i, int j) {
-
-        if (i == j) {
+        if (j == i) {
             return;
         }
 
@@ -39,5 +41,4 @@ public class Main {
         array[i] = array[j];
         array[j] = temp;
     }
-
 }
